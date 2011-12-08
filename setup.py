272462bfab2cb5be51a257403307e9b83b1de1ad
Yuji Tomita <yuchant@gmail.com>
2011-12-08 14:20:06 -0800

    Fixing setup script, adding version number

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 
 setup(
     name="django-jinja2",
-    version=__import__("mailer").__version__,
+    version=__import__("django_jinja2").__version__,
     description="A drop in conditional template loader to use jinja2 if the filename / path meets custom criteria",
     long_description='',
     author="Yuji Tomita",
-    author_email="jtauber@jtauber.com",
+    author_email="yuji@yujitomita.com",
     url="https://github.com/yuchant/django-jinja2",
     packages=[
         "django_jinja2",
